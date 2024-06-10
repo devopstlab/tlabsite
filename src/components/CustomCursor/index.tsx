532026e41e2f08c912c3cf994296829af3a4f591
Rafael Camussi <rafael.camussi@dinoia.com.ar>
2024-06-10 16:04:40 -0300

    fix cursor

--- src/components/CustomCursor/index.tsx
+++ src/components/CustomCursor/index.tsx
@@ -24,10 +24,10 @@ const CustomCursor = () => {
       const mouseX = clientX;
       const mouseY = clientY;
 
-      positionRef.current.mouseX = mouseX - cursorSm.current?.clientWidth / 2;
-      positionRef.current.mouseY = mouseY - cursorSm.current?.clientHeight / 2;
-      positionRef.current.mouseX = mouseX - cursorLg.current?.clientWidth / 2;
-      positionRef.current.mouseY = mouseY - cursorLg.current?.clientHeight / 2;
+      positionRef.current.mouseX = mouseX - (cursorSm.current as any)?.clientWidth / 2;
+      positionRef.current.mouseY = mouseY - (cursorSm.current as any)?.clientHeight / 2;
+      positionRef.current.mouseX = mouseX - (cursorSm.current as any)?.clientWidth / 2;
+      positionRef.current.mouseY = mouseY - (cursorSm.current as any)?.clientHeight / 2;
     });
 
     return () => {};
